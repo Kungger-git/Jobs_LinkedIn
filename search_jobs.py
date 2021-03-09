8b56b15a07c51e1e5601bfca7cc24e4b53e668a3
KungPaoChick <matthewriel63@gmail.com>
2021-03-09 12:40:44 +0000

    going deeper

--- search_jobs.py
+++ search_jobs.py
@@ -2,17 +2,37 @@ import requests, colorama
 from bs4 import BeautifulSoup as soup
 
 
+def write_data(data):
+    title = data[0]
+    location = data[1]
+    with open('jobs.csv', 'a', encoding='utf-8') as f:
+        f.write(f'{title},{location}\n')
+
+
 def scrape_contents(locator):
+    my_data = []
+    # Topcard scraping
     for content in locator.findAll('div', {'class':'topcard__content-left'})[0:]:
         for title in content.findAll('h1', {'class':'topcard__title'})[0:]:
             print(colorama.Fore.GREEN,
-                f'[*] {title.text}', colorama.Style.RESET_ALL)
+               f'[*] {title.text}', colorama.Style.RESET_ALL)
+            my_data.append(title.text)
+        for location in content.findAll('span', {'class':'topcard__flavor topcard__flavor--bullet'})[0:]:
+            print(location.text)
+            my_data.append(location.text.replace(',','.'))
+
+    # Criteria scraping
+    for criteria in locator.findAll('li', {'class':'job-criteria__item'})[0:]:
+        for criteria_content in criteria.findAll('span', {'class':'job-criteria__text job-criteria__text--criteria'})[0:]:
+            print(criteria_content.text)
+
+    write_data(my_data)
 
 
 def parse_job_page(links):
     try:
         print(colorama.Fore.YELLOW,
-            f'[!] There are {len(links)} available jobs.',
+            f'[!] There are {len(links)} available jobs.\n',
             colorama.Style.RESET_ALL)
         for job_link in links:
             job_req = requests.get(job_link)
@@ -35,6 +55,10 @@ def extract_jobs(cursor):
 
 
 def web_parsing():
+    with open('jobs.csv', 'w', encoding='utf-8') as f:
+        headers = 'Job Title, Location, Seniority Level, Employment Type, Job Function, Industries\n'
+        f.write(headers)
+    
     country = str(input('Enter your country/city: '))
     try:
         req = requests.get(
