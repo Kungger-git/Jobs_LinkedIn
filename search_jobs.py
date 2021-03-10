cf1288e096598f37f40d0c8f46e5f940db7a05c3
KungPaoChick <matthewriel63@gmail.com>
2021-03-10 11:08:18 +0000

    📦 Added new features to see how many applicants have been hired, and time postes. Some bugs fixed when Company name and Job Location is mixed up

--- search_jobs.py
+++ search_jobs.py
@@ -29,7 +29,7 @@ def scrape_write(links):
             colorama.Style.RESET_ALL)
 
         with open(f'jobs_in_{place}.csv', 'w', encoding='utf-8') as f:
-            headers = ['Source', 'Organization', 'Job Title', 'Location', 'Posted', 'Seniority Level',
+            headers = ['Source', 'Organization', 'Job Title', 'Location', 'Posted', 'Applicants Hired' , 'Seniority Level',
                        'Employment Type', 'Job Function', 'Industries']
             write = csv.writer(f, dialect='excel')
             write.writerow(headers)
@@ -44,21 +44,17 @@ def scrape_write(links):
 
                 # Topcard scraping
                 for content in job_soup.findAll('div', {'class':'topcard__content-left'})[0:]:
-                    # TODO: work in progress
-                    #orgs = {'Default-Org' : [org.text for org in content.findAll('a', {'class':'topcard__org-name-link topcard__flavor--black-link'})],
-                    #        'Flavor-Org' : [org.text for org in content.findAll('span', {'class':'topcard__flavor'})]}
-                    #        
-                    #if orgs['Default-Org'] == []:
-                    #    my_data.append(orgs['Flavor-Org'][0])
-                    #elif len(orgs['Flavor-Org']) == 2:
-                    #    for org in orgs['Flavor-Org']:
-                    #        my_data.append(orgs['Flavor-Org'][0])
-                    #else:
-                    #    for org in orgs['Default-Org']:
-                    #        my_data.append(org)
+                    # Scraping Organization Names
+                    orgs = {'Default-Org' : [org.text for org in content.findAll('a', {'class':'topcard__org-name-link topcard__flavor--black-link'})],
+                            'Flavor-Org' : [org.text for org in content.findAll('span', {'class':'topcard__flavor'})]}
 
-                    for org in content.findAll('a', {'class':'topcard__org-name-link topcard__flavor--black-link'})[0:]:
-                        my_data.append(org.text)
+                    if orgs['Default-Org'] == []:
+                        my_data.append(orgs['Flavor-Org'][0])
+                    else:
+                        for org in orgs['Default-Org']:
+                            my_data.append(org)              
+
+                    # Scraping Job Title
                     for title in content.findAll('h1', {'class':'topcard__title'})[0:]:
                         print(colorama.Fore.GREEN,
                             f'[*] {title.text}', colorama.Style.RESET_ALL)
@@ -67,6 +63,7 @@ def scrape_write(links):
                     for location in content.findAll('span', {'class':'topcard__flavor topcard__flavor--bullet'})[0:]:
                         my_data.append(location.text.replace(',','.'))
                     
+                    # Scraping Job Time Posted
                     posts = {'Old': [posted.text for posted in content.findAll('span', {'class':'topcard__flavor--metadata posted-time-ago__text'})],
                              'New' : [posted.text for posted in content.findAll('span', {'class':'topcard__flavor--metadata posted-time-ago__text posted-time-ago__text--new'})]}
                     
@@ -77,8 +74,16 @@ def scrape_write(links):
                         for text in posts['New']:
                             my_data.append(text)
 
-                    for applicant in content.findAll('span', {'class':'topcard__flavor--metadata topcard__flavor--bullet num-applicants__caption'})[0:]:
-                        print(applicant.text)
+                    # Scraping Number of Applicants Hired
+                    applicants = {'More-Than' : [applicant.text for applicant in content.findAll('figcaption', {'class':'num-applicants__caption'})],
+                                  'Current' : [applicant.text for applicant in content.findAll('span', {'class':'topcard__flavor--metadata topcard__flavor--bullet num-applicants__caption'})]}
+
+                    if applicants['Current'] == []:
+                        for applicant in applicants['More-Than']:
+                            my_data.append(f'{get_nums(applicant)}+ Applicants')
+                    else:
+                        for applicant in applicants['Current']:
+                            my_data.append(f'{get_nums(applicant)} Applicants')
 
                 # Criteria scraping
                 for criteria in job_soup.findAll('span', {'class':'job-criteria__text job-criteria__text--criteria'})[:4]:
@@ -88,6 +93,13 @@ def scrape_write(links):
     except requests.HTTPError as err:
         print(colorama.Fore.RED,
             f'[!!] Something went wrong! {err}', colorama.Style.RESET_ALL)
+
+
+def get_nums(string):
+    a_list = string.split()
+    for num in a_list:
+        if num.isdigit():
+            return num
 
 
 def extract_jobs(cursor):
