49a4e6c83eeebe460b612a7457e43668182c89ee
KungPaoChick <matthewriel63@gmail.com>
2021-03-10 21:18:19 +0800

    Added read_data feature to display data frame in terminal.

---
+++ read_data.py
@@ -0,0 +1,24 @@
+import pandas as pd
+import os
+
+
+def read():
+    for root, dirs, files in os.walk(os.getcwd()):
+        dirs = dirs
+        for data in files:
+            if data.endswith('.csv'):
+                df = pd.read_csv(os.path.join(root, data))
+                pd.set_option('display.max_rows', None)
+                df.drop('Source', axis=1, inplace=True)
+                print(df)
+
+
+def read_scraped(filename):
+    for root, dirs, files in os.walk(os.getcwd()):
+        dirs = dirs
+        for data in files:
+            if filename == data:
+                df = pd.read_csv(os.path.join(root, data))
+                pd.set_option('display.max_rows', None)
+                df.drop('Source', axis=1, inplace=True)
+                print(f'Data Frame:\n\n{df}')
