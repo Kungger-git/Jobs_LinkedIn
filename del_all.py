58a8dbd1b54cf7ce7de591f00585ea2a0aa2b785
KungPaoChick <matthewriel63@gmail.com>
2021-03-13 19:09:38 +0000

    📦 Removed Anxiety control code 😖. This script works well after testing a bunch of times.

--- del_all.py
+++ del_all.py
@@ -4,41 +4,27 @@
 
 import os
 import shutil
+import colorama
 
 
 def del_recs():
-    # This walk block is just an anxiety control, to make sure not all dependencies don't get deleted by this file
-    repo_content = []
-    for root, dirs, files in os.walk(os.getcwd()):
-        for dir in dirs:
-            for folder in repo_folders:
-                if folder == dir:
-                    #print(f'{folder} is a folder in the repo')
-                    repo_content.append(folder)
-        for f in files:
-            for file in repo_files:
-                if file == f:
-                    #print(f'{file} is file in the repo')
-                    repo_content.append(file)
-    
-    #print(f'{len(repo_content)} total content')
-
     deleted = []
     for content in os.listdir(os.getcwd()):
-        if content in repo_content:
-            continue
-            #print(f'{content} will not get deleted')
-        else:
+        if not content in repo_content:
             deleted.append(content)
             shutil.rmtree(content)
     
-    for x in deleted:
-        print(f'{x} has been deleted')
+    if not deleted == []:
+        for x in deleted:
+            print(f'{x} has been deleted')
+    else:
+        print(colorama.Fore.GREEN,
+            '[*] Repository is clean. Nothing to delete here.',
+            colorama.Style.RESET_ALL)
 
 
 if __name__ == '__main__':
-    repo_files = ['.gitignore', 'LICENSE', 'README.md', 'read_data.py',
+    colorama.init()
+    repo_content = ['img', '.git', '.gitignore', 'LICENSE', 'README.md', 'read_data.py',
                     'requirements.txt', 'search_jobs.py', 'del_all.py']
-    repo_folders = ['img', '.git']
-
     del_recs()
